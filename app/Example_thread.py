48af84b6bd542964f885d99584f3f8eed268d557
ferrariromano <ferrariromano74@gmail.com>
2023-04-05 14:12:01 +0700

    final kommit

---
+++ app/Example_thread.py
@@ -0,0 +1,46 @@
+from threading import Thread
+import time
+
+#olah data 1 juta
+class olah_data:
+    def __init__(self, rentang):
+        self.rentang = rentang
+    
+    def take(self):
+        print("[1] take data from data to : {}".format(self.rentang))
+        time.sleep(1)
+
+    def sort(self):
+        print("[2] sort data from data to : {}".format(self.rentang))
+        time.sleep(1)
+
+    def export(self):
+        print("[3] export data from data to : {}".format(self.rentang))
+        time.sleep(1)
+
+    def run(self):
+        self.take()
+        self.sort()
+        self.export()
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    rentangs = [
+        "1 - 100000",
+        "100001 - 200000",
+        "200001 - 300000",
+        "300001 - 400000",
+        "400001 - 500000",
+        "500001 - 600000",
+        "600001 - 700000",
+        "700001 - 800000",
+        "800001 - 900000",
+        "900001 - 1000000",
+    ]
+
+    for rentang in rentangs:
+        # olah_data(rentang).run()
+        threadActive = Thread(target=olah_data(rentang).run)
+        threadActive.start()
+    finish = time.perf_counter()
+    print("Total waktu = {} detik".format(finish-start))
